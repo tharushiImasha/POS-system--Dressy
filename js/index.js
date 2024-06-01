ad5aa25caa0abc0bf74c355934a97e0475f8860d
tharushiImasha <tharushi.byb@gmail.com>
2024-06-01 15:47:19 +0300

    create orders

--- js/index.js
+++ js/index.js
@@ -7,9 +7,22 @@ window.addEventListener('scroll', function() {
     }
 });
 
+
+window.addEventListener('scroll', function() {
+    var section = document.querySelector('.order-top');
+    var rect = section.getBoundingClientRect();
+    
+    if (rect.top <= 0 && rect.bottom >= window.innerHeight) {
+        section.classList.add('sticky');
+    } else {
+        section.classList.remove('sticky');
+    }
+});
+
 document.getElementById("home-page").style.display = "block";
 document.getElementById("customers-page").style.display = "none";
 document.getElementById("items-page").style.display = "none";
+document.getElementById("orders-page").style.display = "none";
 
 document.getElementById("home").addEventListener("click", function(event){
 
@@ -18,19 +31,28 @@ document.getElementById("home").addEventListener("click", function(event){
     document.getElementById("home-page").style.display = "block";
     document.getElementById("customers-page").style.display = "none";
     document.getElementById("items-page").style.display = "none";
+    document.getElementById("orders-page").style.display = "none";
 })
 
 document.getElementById("items").addEventListener("click", function(){
     document.getElementById("home-page").style.display = "none";
     document.getElementById("customers-page").style.display = "none";
     document.getElementById("items-page").style.display = "block";
+    document.getElementById("orders-page").style.display = "none";
 })
 
 document.getElementById("customers").addEventListener("click", function(){
     document.getElementById("home-page").style.display = "none";
     document.getElementById("customers-page").style.display = "block";
     document.getElementById("items-page").style.display = "none";
+    document.getElementById("orders-page").style.display = "none";
 })
 
+document.getElementById("orders").addEventListener("click", function(){
+    document.getElementById("home-page").style.display = "none";
+    document.getElementById("customers-page").style.display = "none";
+    document.getElementById("items-page").style.display = "none";
+    document.getElementById("orders-page").style.display = "block";
+})
 
 
