57ec4a17b1856b2924f2fc0f87417e305adf49ff
tharushiImasha <tharushi.byb@gmail.com>
2024-06-13 21:58:50 +0300

    make menu

--- js/index.js
+++ js/index.js
@@ -69,3 +69,14 @@ document.getElementById("checkOrders").addEventListener("click", function(){
 })
 
 
+let menuList = document.getElementById("menu-list")
+menuList.style.maxHeight = "0px";
+
+function toggleMenu(){
+    if(menuList.style.maxHeight == "0px"){
+        menuList.style.maxHeight = "300px";
+    }
+    else{
+        menuList.style.maxHeight = "0px"
+    }
+}
