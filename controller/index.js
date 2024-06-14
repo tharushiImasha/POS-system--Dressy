6f6205b7d262a2781e454537186d7ac4d47eee26
tharushiImasha <tharushi.byb@gmail.com>
2024-06-14 22:34:36 +0300

    meke menu dropdown

--- controller/index.js
+++ controller/index.js
@@ -69,14 +69,46 @@ document.getElementById("checkOrders").addEventListener("click", function(){
 })
 
 
-let menuList = document.getElementById("menu-list")
-menuList.style.maxHeight = "0px";
 
-function toggleMenu(){
-    if(menuList.style.maxHeight == "0px"){
-        menuList.style.maxHeight = "300px";
-    }
-    else{
-        menuList.style.maxHeight = "0px"
-    }
-}
+document.getElementById("items-m").addEventListener("click", function(){
+    document.getElementById("home-page").style.display = "none";
+    document.getElementById("customers-page").style.display = "none";
+    document.getElementById("items-page").style.display = "block";
+    document.getElementById("orders-page").style.display = "none";
+    document.getElementById("all-orders").style.display = "none";
+})
+
+document.getElementById("customers-m").addEventListener("click", function(){
+    document.getElementById("home-page").style.display = "none";
+    document.getElementById("customers-page").style.display = "block";
+    document.getElementById("items-page").style.display = "none";
+    document.getElementById("orders-page").style.display = "none";
+    document.getElementById("all-orders").style.display = "none";
+})
+
+document.getElementById("orders-m").addEventListener("click", function(){
+    document.getElementById("home-page").style.display = "none";
+    document.getElementById("customers-page").style.display = "none";
+    document.getElementById("items-page").style.display = "none";
+    document.getElementById("orders-page").style.display = "block";
+    document.getElementById("all-orders").style.display = "none";
+})
+
+document.getElementById("checkOrders-m").addEventListener("click", function(){
+    document.getElementById("home-page").style.display = "none";
+    document.getElementById("customers-page").style.display = "none";
+    document.getElementById("items-page").style.display = "none";
+    document.getElementById("orders-page").style.display = "none";
+    document.getElementById("all-orders").style.display = "block";
+})
+
+document.getElementById("home-m").addEventListener("click", function(event){
+
+    event.preventDefault();
+
+    document.getElementById("home-page").style.display = "block";
+    document.getElementById("customers-page").style.display = "none";
+    document.getElementById("items-page").style.display = "none";
+    document.getElementById("orders-page").style.display = "none";
+    document.getElementById("all-orders").style.display = "none";
+})
